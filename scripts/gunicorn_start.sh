db6dabd6d02e52d001417974e448d86c04f822f2
mike <mike@atlaswearables.com>
2017-03-12 18:02:32 -0500

    correct virtualenv directory

--- scripts/gunicorn_start.sh
+++ scripts/gunicorn_start.sh
@@ -13,7 +13,7 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source ../bin/activate
+source /home/indabom/web/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
