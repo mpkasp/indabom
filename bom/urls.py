adf61e766faa443936150fb269d58615d7abd52d
mike <mike@atlaswearables.com>
2017-03-18 01:18:12 -0500

    url error

--- bom/urls.py
+++ bom/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
     url(r'^(?P<part_id>[0-9]+)/$', views.indented, name='indented'),
     url(r'^(?P<part_id>[0-9]+)/export/$', views.export_part_indented, name='export-part-indented'),
     url(r'^(?P<part_id>[0-9]+)/upload/$', views.upload_part_indented, name='upload-part-indented'),
