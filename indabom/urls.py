926c681e08fce46d09d94751282e3b15b644bbb3
mike <mike@atlaswearables.com>
2017-04-05 23:09:11 -0500

    styling and login logic

--- indabom/urls.py
+++ indabom/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^bom/', include('bom.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, {'template_name': 'indabom/login.html'}, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'indabom/login.html', 'redirect_authenticated_user': True}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
