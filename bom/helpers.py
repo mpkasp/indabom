3bd8b91586c766a7edb75bd8ae18a3323138f0b9
mike <mike@atlaswearables.com>
2017-04-06 23:37:21 -0500

    try to fix styling

--- bom/helpers.py
+++ bom/helpers.py
@@ -3,3 +3,5 @@ from bom.models import Part
 
 def create_some_fake_parts:
     pt1 = Part(manufacturer_part_number='GRM1555C1H100JA01D')
+    pt2 = Part(manufacturer_part_number='STM32F401CEU6')
+    pt3 = Part(manufacturer_part_number='NRF51822')
