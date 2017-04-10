e9cf5d3416d0a42e208f5ab0c0ed1d68996f2e76
mike <mike@atlaswearables.com>
2017-04-10 13:06:19 -0500

    redirect from index if you're logged in

--- indabom/views.py
+++ indabom/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.template.response import TemplateResponse
 from django.db import IntegrityError
 
 def index(request):
-    return TemplateResponse(request, 'indabom/index.html', locals())
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/bom/')
+    else:
+        return TemplateResponse(request, 'indabom/index.html', locals())
