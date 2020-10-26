9c6346b7b343d409308c03df1a417b483dd05347
Mike Kasparian <mpkasp@gmail.com>
2020-10-25 23:14:12 -0500

    bump dbom

--- bump_dbom.sh
+++ bump_dbom.sh
@@ -2,5 +2,10 @@
 pip freeze > requirements.txt
 git add --all
 git commit -am "bump dbom"
-git push
-./deploy.sh
+git status
+read -p "Continue? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  git push
+  ./deploy.sh
+fi
