53f612b583a7d201cbbe750dc6b4d64b897b0b98
Mike Kasparian <mpkasp@gmail.com>
2021-06-13 17:36:49 -0400

    fix copyright year, upgrade packages

--- indabom/settings.py
+++ indabom/settings.py
@@ -82,7 +82,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'indabom.wsgi.application'
-
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
