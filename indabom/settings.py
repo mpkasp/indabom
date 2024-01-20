3740e0d7fe0cf1122030f86a6fc1fb6a79ec6d8a
Michael Kasparian <mpkasp@gmail.com>
2024-01-20 13:29:04 -0500

    add readonly db

--- indabom/settings.py
+++ indabom/settings.py
@@ -278,6 +278,10 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        },
+        'readonly': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
