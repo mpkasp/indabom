7daac46e6fde19db4602a8e893c792b023edd16c
Michael Kasparian <mpkasp@gmail.com>
2024-02-19 19:27:13 -0500

    superuser password

---
+++ indabom/migrations/0001_createsuperuser.py
@@ -0,0 +1,40 @@
+import os
+
+from django.contrib.auth.models import User
+from django.db import migrations
+from django.db.backends.postgresql.schema import DatabaseSchemaEditor
+from django.db.migrations.state import StateApps
+
+import google.auth
+from google.cloud import secretmanager
+
+
+def createsuperuser(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
+    """
+    Dynamically create an admin user as part of a migration
+    Password is pulled from Secret Manger (previously created as part of tutorial)
+    """
+    try:
+        client = secretmanager.SecretManagerServiceClient()
+
+        # Get project value for identifying current context
+        _, project = google.auth.default()
+
+        # Retrieve the previously stored admin password
+        PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
+        name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
+        admin_password = client.access_secret_version(name=name).payload.data.decode(
+            "UTF-8"
+        )
+    except:
+        admin_password = 'test'
+
+    # Create a new user using acquired password, stripping any accidentally stored newline characters
+    User.objects.create_superuser("admin", password=admin_password.strip())
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+    dependencies = []
+    operations = [migrations.RunPython(createsuperuser)]
